f419cde843fe33bbd2966223745977ee1a42918d
chihiro1234567 <chihirodoi@gmail.com>
2023-10-31 00:26:14 +0900

    add uart

---
+++ ch32v003-uart/README.md
@@ -0,0 +1,11 @@
+# CH32V003 UART
+
+
+
+
+## ビルド＆実行方法
+
+```bash
+> make clean && make && ../ch32v003fun/minichlink/minichlink -T
+:
+```
---
+++ ch32v003-uart/uart.c
@@ -0,0 +1,19 @@
+/* Template app on which you can build your own. */
+
+#include "ch32v003fun.h"
+#include <stdio.h>
+
+uint32_t count;
+
+int main()
+{
+	SystemInit();
+
+
+	while(1)
+	{
+		
+		Delay_Ms(250);
+	}
+}
+
